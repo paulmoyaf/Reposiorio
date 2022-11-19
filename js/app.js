a2ba42156264b9178dd91ef253c57296ca7ea8f7
Paul Moya <paul.moyaf@gmail.com>
2022-11-19 13:38:24 +0100

    Cambios para version 2

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ console.log('Paul console log');
 console.log('Esta es una pruba para nueva branch/rama');  
 
 const saludos = () =>{
-    console.log('Hola Mundo 2');
+    alert('Hola Mundo 2');
 }
 
